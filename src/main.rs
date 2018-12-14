43ba6b34fee33b28825fb518efccc30ef60da196
lukehedger <luke.hedger@gmail.com>
2018-12-14 13:19:05 +1100

    🔩

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use std::fs;
+
+fn main() {
+    println!("Hello, wordl!");
+
+    let alphabet: String = fs::read_to_string("alphabet.txt").expect("alphabet.txt is not found in current directory.");
+    let reverse_alphabet: String = alphabet.chars().rev().collect();
+    fs::write("reverse_alphabet.txt", reverse_alphabet).expect("Could not write reverse alphabet.txt");
+}
